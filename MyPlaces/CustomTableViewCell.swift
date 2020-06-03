e559f293bdaa14ba3d9d695bdcca1ef6250f3ebb
Leonteva Anastasia <lagarnas@gmail.com>
2020-06-03 12:53:37 +0400

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Анастасия Лагарникова on 03.06.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+}
